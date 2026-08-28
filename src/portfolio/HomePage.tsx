b866000bbbb483fbfa63990691fc3e3f72271155
Bryan Cardeno <108753983+BryanCardeno@users.noreply.github.com>
2026-08-27 21:43:24 -0700

    minor changes

--- src/portfolio/HomePage.tsx
+++ src/portfolio/HomePage.tsx
@@ -1,5 +1,13 @@
 const HomePage = () => {
-  return <main className="container">test</main>;
+  return (
+    <main className="container">
+      <div className="book">
+        <div className="cover">
+          <span className="cover-text">Full Stack Developer</span>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default HomePage;
